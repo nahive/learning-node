459f5cd31df930ff700d84b569d8cbfe1080b435
Szymon Maslanka <szymon.maslanka@4pts.co.uk>
2018-04-03 11:16:05 +0200

    added mongodb + server

--- todoapp/dest/controllers/todoController.js
+++ todoapp/dest/controllers/todoController.js
@@ -1,39 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 class TodoController {
     constructor(app) {
         this.parser = bodyParser.urlencoded({ extended: false });
-        this.data = [
-            new Item('get milk'),
-            new Item('walk dong'),
-            new Item('kick some coding ass')
-        ];
+        this.dburl = 'mongodb://test:test@ds062178.mlab.com:62178/nahive_tododb';
         this.app = app;
         this.bind();
+        this.database();
     }
     bind() {
         this.app.get('/todo', (req, res) => this.get(req, res));
         this.app.post('/todo', this.parser, (req, res) => this.post(req, res));
         this.app.delete('/todo/:item', (req, res) => this.delete(req, res));
     }
+    database() {
+        // connect to database
+        mongoose.connect(this.dburl);
+    }
     get(req, res) {
-        console.log(this.data);
-        res.render('todo', { todos: this.data });
+        // getting data from mongo and pass to view
+        Todo.find({}, function (err, data) {
+            if (err)
+                throw err;
+            res.render('todo', { todos: data });
+        });
     }
     post(req, res) {
-        this.data.push(new Item(req.body.value));
-        res.json(this.data);
+        // get data from view & add to mongo)
+        new Item(req.body.value).model().save(function (err, data) {
+            if (err)
+                throw err;
+            res.json(data);
+        });
     }
     delete(req, res) {
-        this.data = this.data.filter(function (todo) {
-            return todo.name.replace(/ /g, '-') !== req.params.item;
+        // delete requested item from mongo
+        Todo.find({ name: req.params.item.replace(/\-/g, " ") }).remove(function (err, data) {
+            if (err)
+                throw err;
+            res.json(data);
         });
-        res.json(this.data);
     }
 }
 exports.TodoController = TodoController;
+// schema for db
+var Todo = mongoose.model('Todo', new mongoose.Schema({ name: String }));
 class Item {
+    model() {
+        return new Todo({ name: this.name });
+    }
     constructor(name) {
         this.name = name;
     }
--- todoapp/src/controllers/todoController.ts
+++ todoapp/src/controllers/todoController.ts
@@ -1,20 +1,17 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
+import * as mongoose from 'mongoose'
 
 export class TodoController {
     
     private app: express.Express
     private parser = bodyParser.urlencoded({extended: false})
-
-    private data: Item[] = [
-        new Item('get milk'),
-        new Item('walk dong'),
-        new Item('kick some coding ass')
-    ]
+    private dburl = 'mongodb://test:test@ds062178.mlab.com:62178/nahive_tododb'
 
     constructor(app: express.Express) {
         this.app = app
         this.bind()
+        this.database()
     }
 
     private bind() {
@@ -23,26 +20,45 @@ export class TodoController {
         this.app.delete('/todo/:item', (req, res) => this.delete(req, res))
     }
 
+    private database() {
+        // connect to database
+        mongoose.connect(this.dburl)
+    }
+
     private get(req: express.Request, res: express.Response) {
-        console.log(this.data)
-        res.render('todo', {todos: this.data})
+        // getting data from mongo and pass to view
+        Todo.find({}, function(err, data) {
+            if (err) throw err
+            res.render('todo', {todos: data})
+        })
     }
 
     private post(req: express.Request, res: express.Response) {
-        this.data.push(new Item(req.body.value))
-        res.json(this.data);
+        // get data from view & add to mongo)
+        new Item(req.body.value).model().save(function(err, data) {
+            if (err) throw err
+            res.json(data);
+        })
     }
 
     private delete(req: express.Request, res: express.Response) {
-        this.data = this.data.filter(function(todo) {
-            return todo.name.replace(/ /g, '-') !== req.params.item
+        // delete requested item from mongo
+        Todo.find({name: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+            if (err) throw err
+            res.json(data)
         })
-        res.json(this.data)
     }
 }
 
+// schema for db
+var Todo = mongoose.model('Todo', new mongoose.Schema({ name: String }))
+
 class Item {
     public name: string
+
+    public model(): mongoose.Document {
+        return new Todo({name: this.name})
+    }
 
     constructor(name: string) {
         this.name = name
